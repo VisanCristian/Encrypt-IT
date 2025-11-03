3535884c37d43d3dc75fdf1b0cd011e89ce784a3
Andrei <c.andrey2309@gmai.com>
2025-11-03 21:18:01 +0200

    added some GUI elements

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLa
 from gui.main_ui import Ui_Dialog
 import sys
 from library.encrypt import encrypt, decrypt
-
+#
 class MyDialog(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
